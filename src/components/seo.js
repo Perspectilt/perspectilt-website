3395dfdee7f34409411dfaed45876d88ad0e54c9
Syrus Dark <naveen@syrusdark.website>
2020-05-24 19:46:46 +0530

    Some logo thing

--- src/components/seo.js
+++ src/components/seo.js
@@ -71,16 +71,25 @@ function SEO({ description, lang, meta, title }) {
       ].concat(meta)}
     />
     <Helmet>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=jwEXOW40pg" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=jwEXOW40pg" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=jwEXOW40pg" />
+        <link rel="manifest" href="/site.webmanifest?v=jwEXOW40pg" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg?v=jwEXOW40pg" color="#5bbad5" />
+        <link rel="shortcut icon" href="/favicon.ico?v=jwEXOW40pg" />
+        <meta name="msapplication-TileColor" content="#603cba" />
+        <meta name="theme-color" content="#ffffff" />
         <script type="application/ld+json">
         {String(`{
           "@context": "https://schema.org",
           "@type": "Organization",
           "url": "https://perspectilt.js.org/",
+          "logo": "https://perspectilt.js.org/logo.png",
           "name": "Perspectilt",
           "contactPoint": {
             "@type": "ContactPoint",
             "email": "perspectilt@syrusdark.website",
-            "contactType": "Random"
+            "contactType": "Important"
           }
         }`
       )
