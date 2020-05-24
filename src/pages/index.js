37b34d16c812dca25992a807237b0373bb0bdb08
Syrus Dark <naveen@syrusdark.website>
2020-05-24 17:43:24 +0530

    Port to GatsBy.

---
+++ src/pages/index.js
@@ -0,0 +1,23 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Pespectilt." lang="en"/>
+    <h1>We are perspectilt.</h1>
+    <p>We do many things and are build the site. So you could see many updates. For now we started with a Gatsby template.</p>
+    <p>From now on, we will build this site Good and Ready to show up.</p>
+    <p> Check out our terminal interface. We would be build next.</p>
+    <Link to="/terminal/">Go to Terminal</Link>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div>
+    <Link to="/page-2/">Go to page 2</Link>
+  </Layout>
+)
+
+export default IndexPage
---
+++ src/pages/terminal.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import Terminal from 'terminal-in-react';
+import SEO from "../components/seo"
+
+class App extends Component {
+  showMsg = () => 'Hello World'
+
+  render() {
+    return (
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh"
+        }}
+      >
+        <SEO title="Terminal Interface" description="Our terminal Interface. Currently testing."/>
+        <Terminal
+          color='green'
+          backgroundColor='black'
+          barColor='black'
+          style={{ fontWeight: "bold", fontSize: "1em" }}
+          commands={{
+            'open-google': () => window.open('https://www.google.com/', '_blank'),
+            showmsg: this.showMsg,
+            popup: () => alert('Terminal in React')
+          }}
+          descriptions={{
+            'open-google': 'opens google.com',
+            showmsg: 'shows a message',
+            alert: 'alert', popup: 'alert'
+          }}
+          msg='You can write anything here. Example - Hello! My name is Foo and I like Bar.'
+        />
+      </div>
+    );
+  }
+}
+export default App;
